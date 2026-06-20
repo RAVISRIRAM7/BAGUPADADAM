90925e85971b25aa609cb5067352759cfb628b8b
hameed386 <hameedbheemanapati38@gmail.com>
2026-06-20 23:08:18 +0530

    added clear changes

--- Datatypes.java
+++ Datatypes.java
@@ -1,5 +1,5 @@
 public class Datatypes{
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello World and learn Datatypes in Java@@@@@");
     }
 }
--- Inheritence.java
+++ Inheritence.java
@@ -1,5 +1,5 @@
 public class Inheritence {
     public static void main(String[] args) {
-        System.out.println("Learning Inheritence in Java");
+        System.out.println("Learning Inheritence in Java@@@@@");
     }
 }
