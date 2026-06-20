2d5eb49768d3f71cb0f62128ca095c12d5e33df3
hameed386 <hameedbheemanapati38@gmail.com>
2026-06-20 23:04:36 +0530

    thrice

--- Hello.java
+++ Hello.java
@@ -1,13 +1,7 @@
 class Hello {
     public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         System.err.println("BAGUPADADM Hello world inkenti choosukundham!");
-    }hity 
-=======
-=======
->>>>>>> main
-        System.out.println("BAGUPADADM Hello world!");
     }
->>>>>>> c9602d72f91f31f12ce9139e9398b24a4bffa987
+
 }
